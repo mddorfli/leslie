a515e5789d8c076111e9ea71bd0c64eba3a1a2a4
Marco Dörfliger <marco.dorfliger@imtech.com>
2017-03-23 13:07:33 +0100

    Renamed URLs, fixed build, removed encryptino from client/server comms

--- leslie.ui.html/src/main/java/org/leslie/ui/html/DataSourceCredentialVerifier.java
+++ leslie.ui.html/src/main/java/org/leslie/ui/html/DataSourceCredentialVerifier.java
@@ -38,128 +38,146 @@ public class DataSourceCredentialVerifier implements ICredentialVerifier {
 	    + " WHERE username = ? "
 	    + "   AND id != 1 ";
 
-    private static final int DEFAULT_MAX_RETRIES = 5;
+	private static final int DEFAULT_MAX_RETRIES = 5;
 
-    private static Logger logger = LoggerFactory.getLogger(DataSourceCredentialVerifier.class);
+	private static Logger logger = LoggerFactory.getLogger(DataSourceCredentialVerifier.class);
 
-    private String m_dbUrl;
-    private String m_dbUsername;
-    private String m_dbPassword;
-    private Integer m_maxRetries;
+	private String m_dbUrl;
+	private String m_dbUsername;
+	private String m_dbPassword;
+	private String m_driverClass;
+	private Integer m_maxRetries;
 
-    private Connection m_connection;
+	private Connection m_connection;
 
-    @PostConstruct
-    private void init() {
-	m_dbUrl = BEANS.get(JdbcUrlProperty.class).getValue();
-	m_dbUsername = BEANS.get(JdbcUsernameProperty.class).getValue();
-	m_dbPassword = BEANS.get(JdbcPasswordProperty.class).getValue();
-	m_maxRetries = BEANS.get(MaxRetriesProperty.class).getValue();
+	@PostConstruct
+	private void init() {
+		m_dbUrl = BEANS.get(JdbcUrlProperty.class).getValue();
+		m_driverClass = BEANS.get(JdbcDriverClassProperty.class).getValue();
+		m_dbUsername = BEANS.get(JdbcUsernameProperty.class).getValue();
+		m_dbPassword = BEANS.get(JdbcPasswordProperty.class).getValue();
+		m_maxRetries = BEANS.get(MaxRetriesProperty.class).getValue();
 
-	try {
-	    m_connection = DriverManager.getConnection(m_dbUrl, m_dbUsername, m_dbPassword);
-	} catch (SQLException e) {
-	    logger.error("Unable to connect to database", e);
+		if (m_driverClass != null) {
+			try {
+				Class.forName(m_driverClass);
+			} catch (ClassNotFoundException e) {
+				logger.error("Class '"+m_driverClass+"' could not be initialised.", e);
+			}
+		}
+		
+		try {
+			m_connection = DriverManager.getConnection(m_dbUrl, m_dbUsername, m_dbPassword);
+		} catch (SQLException e) {
+			logger.error("Unable to connect to database", e);
+		}
 	}
-    }
 
-    @PreDestroy
-    private void destroy() {
-	if (m_connection != null) {
-	    try {
-		m_connection.close();
-	    } catch (SQLException e) {
-		// NOP
-	    }
-	}
-
-    }
-
-    @Override
-    public int verify(String username, char[] password) throws IOException {
-	int result = AUTH_FORBIDDEN;
-	try (PreparedStatement stmt = m_connection.prepareStatement(SELECT_USER_SQL)) {
-	    stmt.setString(1, username);
-	    stmt.setInt(2, m_maxRetries != null ? m_maxRetries.intValue() : DEFAULT_MAX_RETRIES);
-
-	    logger.debug("{}", stmt);
-
-	    stmt.execute();
-	    ResultSet rs = stmt.getResultSet();
-	    if (rs != null && rs.next()) {
-		String pwHash = rs.getString(1);
-		String pwSalt = rs.getString(2);
-
-		if (!StringUtility.isNullOrEmpty(pwHash) && !StringUtility.isNullOrEmpty(pwSalt)) {
-		    byte[] hash = SecurityUtility.hash(Base64Utility.decode(String.valueOf(password)),
-			    Base64Utility.decode(pwSalt));
-		    if (Arrays.equals(hash, Base64Utility.decode(pwHash))) {
-			result = AUTH_OK;
-		    }
+	@PreDestroy
+	private void destroy() {
+		if (m_connection != null) {
+			try {
+				m_connection.close();
+			} catch (SQLException e) {
+				// NOP
+			}
 		}
 
-	    }
-
-	} catch (SQLException e) {
-	    logger.error("Unable to fetch authentication info from db", e);
-	    result = AUTH_FAILED;
 	}
 
-	if (result == AUTH_FAILED && !StringUtility.isNullOrEmpty(username)) {
-	    // attempt to increment the failed retry count
-	    try (PreparedStatement stmt = m_connection.prepareStatement(UPDATE_RETRY_COUNT_SQL)) {
-		stmt.setString(1, username);
-		int updates = stmt.executeUpdate();
-		if (updates > 0) {
-		    logger.info("retry count for {} incremented. {} row updated.", username, updates);
+	@Override
+	public int verify(String username, char[] password) throws IOException {
+		int result = AUTH_FORBIDDEN;
+		try (PreparedStatement stmt = m_connection.prepareStatement(SELECT_USER_SQL)) {
+			stmt.setString(1, username);
+			stmt.setInt(2, m_maxRetries != null ? m_maxRetries.intValue() : DEFAULT_MAX_RETRIES);
+
+			logger.debug("{}", stmt);
+
+			stmt.execute();
+			ResultSet rs = stmt.getResultSet();
+			if (rs != null && rs.next()) {
+				String pwHash = rs.getString(1);
+				String pwSalt = rs.getString(2);
+
+				if (!StringUtility.isNullOrEmpty(pwHash) && !StringUtility.isNullOrEmpty(pwSalt)) {
+					byte[] hash = SecurityUtility.hash(Base64Utility.decode(String.valueOf(password)),
+							Base64Utility.decode(pwSalt));
+					if (Arrays.equals(hash, Base64Utility.decode(pwHash))) {
+						result = AUTH_OK;
+					}
+				}
+
+			}
+
+		} catch (SQLException e) {
+			logger.error("Unable to fetch authentication info from db", e);
+			result = AUTH_FAILED;
 		}
-	    } catch (SQLException e) {
-		logger.warn("Unable to update retry count for user " + username, e);
-	    }
+
+		if (result == AUTH_FAILED && !StringUtility.isNullOrEmpty(username)) {
+			// attempt to increment the failed retry count
+			try (PreparedStatement stmt = m_connection.prepareStatement(UPDATE_RETRY_COUNT_SQL)) {
+				stmt.setString(1, username);
+				int updates = stmt.executeUpdate();
+				if (updates > 0) {
+					logger.info("retry count for {} incremented. {} row updated.", username, updates);
+				}
+			} catch (SQLException e) {
+				logger.warn("Unable to update retry count for user " + username, e);
+			}
+		}
+
+		return result;
 	}
 
-	return result;
-    }
+	public static class JdbcUrlProperty extends AbstractStringConfigProperty {
 
-    public static class JdbcUrlProperty extends AbstractStringConfigProperty {
-
-	@Override
-	public String getKey() {
-	    return "auth.jdbc.url";
+		@Override
+		public String getKey() {
+			return "auth.jdbc.url";
+		}
 	}
-    }
-
-    public static class JdbcUsernameProperty extends AbstractStringConfigProperty {
-
-	@Override
-	public String getKey() {
-	    return "auth.jdbc.user";
+	
+	public static class JdbcDriverClassProperty extends AbstractStringConfigProperty {
+		
+		@Override
+		public String getKey() {
+			return "auth.jdbc.driverClass";
+		}
 	}
-    }
 
-    public static class JdbcPasswordProperty extends AbstractStringConfigProperty {
+	public static class JdbcUsernameProperty extends AbstractStringConfigProperty {
 
-	@Override
-	public String getKey() {
-	    return "auth.jdbc.password";
+		@Override
+		public String getKey() {
+			return "auth.jdbc.user";
+		}
 	}
-    }
 
-    public static class MaxRetriesProperty extends AbstractPositiveIntegerConfigProperty {
+	public static class JdbcPasswordProperty extends AbstractStringConfigProperty {
 
-	@Override
-	public String getKey() {
-	    return "auth.maxretries";
+		@Override
+		public String getKey() {
+			return "auth.jdbc.password";
+		}
 	}
-    }
 
-    public static void main(String[] args) {
-	String username = "admin";
-	String password = "manager";
-	byte[] salt = SecurityUtility.createRandomBytes();
-	byte[] hash = SecurityUtility.hash(Base64Utility.decode(password), salt);
+	public static class MaxRetriesProperty extends AbstractPositiveIntegerConfigProperty {
 
-	System.out.printf("The salt is: [%s]\n", Base64Utility.encode(salt));
-	System.out.printf("The hash for %s/%s is: [%s]\n", username, password, Base64Utility.encode(hash));
-    }
+		@Override
+		public String getKey() {
+			return "auth.maxretries";
+		}
+	}
+
+	public static void main(String[] args) {
+		String username = "admin";
+		String password = "manager";
+		byte[] salt = SecurityUtility.createRandomBytes();
+		byte[] hash = SecurityUtility.hash(Base64Utility.decode(password), salt);
+
+		System.out.printf("The salt is: [%s]\n", Base64Utility.encode(salt));
+		System.out.printf("The hash for %s/%s is: [%s]\n", username, password, Base64Utility.encode(hash));
+	}
 }
